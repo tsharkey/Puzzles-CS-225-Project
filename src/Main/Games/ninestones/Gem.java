05deea6b8b40b5a1e376d5c479630d69dbdc157e
b-casey <scienttt@gmail.com>
2015-05-02 19:15:28 -0400

    Frontend work on ninestones

--- src/Main/Games/ninestones/Gem.java
+++ src/Main/Games/ninestones/Gem.java
@@ -1,11 +1,9 @@
 package Main.Games.ninestones;
 
-import java.awt.Point;
-
 @SuppressWarnings("serial")
 public class Gem extends Rock {
 
-	public Gem(Point coord) {
-		super(coord, 55);
+	public Gem() {
+		super(55);
 	}
 }
--- src/Main/Games/ninestones/Interface.java
+++ src/Main/Games/ninestones/Interface.java
@@ -1,19 +1,20 @@
 package Main.Games.ninestones;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.Point;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Collections;
-
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 import Main.Games.GamePanel;
 
@@ -21,69 +22,111 @@ import Main.Games.GamePanel;
 @SuppressWarnings("serial")
 public class Interface extends GamePanel {
 	
-	private ArrayList<Rock> rocks;
-	private Gem gem;
-	private JLabel rockDragInfo, moneyTotal, weighStatus, gameInstruction;
-	private JLabel scaleLeftCount, scaleRightCount;
-	private JButton btnWeigh, btnBuy, btClearScale;
-	//private BufferedImage scale;
-	private JPanel gamePanel;
 	private Scale scale;
+	private JTextArea rockSelectInfo;
+	private JPanel rockSelect, buttonPanel;
+	private JButton btnWeigh, btnBuy, btClearScale;
+	
+	//
+	private JLabel moneyTotal, weighStatus, gameInstruction;
+	private JLabel scaleLeftCount, scaleRightCount;
+	
+	
 	private GameLogic game;
 	
 	public Interface() {
+		
 		this.game = new GameLogic();
 		
 		this.setBackground(Color.BLUE);
+		this.setLayout(new GridBagLayout());
 		
-		setLayout(new BorderLayout());
+		constructLayoutComponents();
 		
-		this.gamePanel = new JPanel();
-		// randomizing order of rocks and gold
-		ArrayList<Point> order = new ArrayList<Point>();
-		for (int i = 0, x = 90; i < 9; i++, x+=70) {
-			order.add(new Point(x, 50));
-		}
-		Collections.shuffle(order);
-		
-	//	gamePanel.setSize(Main.Assets.Constants.SCREEN_WIDTH,
-	//			Main.Assets.Constants.SCREEN_HEIGHT);
-		
-		this.rocks = new ArrayList<Rock>();
-		for (int i = 0; i < 8; i++) {
-			rocks.add(new Rock(order.get(i)));
-			rocks.get(i).setPreferredSize(new Dimension(60, 60));
-			gamePanel.add(rocks.get(i));
-		}
-		this.gem = new Gem(order.get(8));
-		gem.setPreferredSize(new Dimension(60, 60));
-		gamePanel.add(gem);
-		
-		this.scale = new Scale();
-		scale.setPreferredSize(new Dimension(800, 600));
-		//scale.setLocation(0, 0);
-		this.add(scale, BorderLayout.SOUTH);
-		
-		JLabel timerLabel = new JLabel("Time Remaining 300 seconds");
-		timerLabel.setLocation(0, 0);
-		timerLabel.setPreferredSize(new Dimension(10,0));
-		System.out.println(timerLabel.getPreferredSize().width);
-		System.out.println(timerLabel.getPreferredSize().height);
-
-		gamePanel.add(timerLabel);
-		gamePanel.setBackground(Color.BLUE);
-		gamePanel.setVisible(true);
-		
-		this.add(gamePanel, BorderLayout.NORTH);
+		/// Adding to frame
+		GridBagConstraints c = new GridBagConstraints();
+	    c.weightx = 0.5;
+	    c.fill = GridBagConstraints.HORIZONTAL;
+	    c.gridx = 0;
+	    c.gridy = 0;
+	    this.add(this.rockSelectInfo, c);
+	 
+	    c.fill = GridBagConstraints.HORIZONTAL;
+	    c.weightx = 0.5;
+	    c.gridx = 1;
+	    c.gridy = 0;
+	    this.add(this.rockSelect, c);
+	 
+	    c.fill = GridBagConstraints.HORIZONTAL;
+	    c.weightx = 0.0;
+	    c.gridwidth = 3;
+	    c.gridx = 1;
+	    c.gridy = 1;
+	    this.add(scale, c);
+	    
+	    c.fill = GridBagConstraints.NONE;
+	    c.weightx = 0.0;
+	    c.gridwidth = 1;
+	    c.gridx = 0;
+	    c.gridy = 1;
+	    this.add(this.buttonPanel, c);
+		////
 		
 		this.setSize(Main.Assets.Constants.SCREEN_WIDTH,
 				Main.Assets.Constants.SCREEN_HEIGHT);
-		
 		this.setVisible(true);
 	}
 	
-	public JPanel getGamePanel() {
-		return this.gamePanel;
+	private void constructLayoutComponents() {
+		// rock instructions
+		this.rockSelectInfo = new JTextArea();
+		this.rockSelectInfo.setPreferredSize(new Dimension(70,50));
+		this.rockSelectInfo.setLineWrap(true);
+		this.rockSelectInfo.setWrapStyleWord(true);
+		this.rockSelectInfo.setEditable(false);
+		this.rockSelectInfo.setBackground(Color.BLUE);
+		this.rockSelectInfo.setText("Drag rocks to the scale to weight them:");
+		Font font = new Font("Verdana", Font.BOLD, 12);
+		this.rockSelectInfo.setFont(font);
+		this.rockSelectInfo.setForeground(Color.RED);
+		
+		// setting up rocks+gem -> random order
+		ArrayList<Rock> rocks = new ArrayList<Rock>();
+		for (int i = 0; i < 8; i++) {
+			rocks.add(new Rock());
+			rocks.get(i).setPreferredSize(new Dimension(60, 60));
+		}
+		Gem gem = new Gem();
+		gem.setPreferredSize(new Dimension(60, 60));
+		rocks.add(gem);
+		Collections.shuffle(rocks);
+		
+		// adding to panel
+		this.rockSelect = new JPanel();
+		this.rockSelect.setPreferredSize(new Dimension(600, 70));
+		for (int i = 0; i < 9; i++) {
+			rockSelect.add(rocks.get(i));
+		}
+		rockSelect.setBackground(Color.BLUE);
+		
+		// constructing scale
+		this.scale = new Scale();
+		scale.setPreferredSize(new Dimension(800, 600));
+		
+		// button panel
+		this.buttonPanel = new JPanel();
+		this.buttonPanel.setLayout(new GridLayout(0,1));
+		btnWeigh = new JButton("$9 WEIGH");
+		btnWeigh.addActionListener(new Interface.ButtonListener());
+		this.buttonPanel.add(btnWeigh);
+		btnBuy = new JButton("$9 PURCHASE");
+		btnBuy.addActionListener(new Interface.ButtonListener());
+		this.buttonPanel.add(btnBuy);
+		btClearScale = new JButton("CLEAR SCALE");
+		btClearScale.addActionListener(new Interface.ButtonListener());
+		this.buttonPanel.add(btClearScale);
+		this.buttonPanel.setPreferredSize(new Dimension(125, 100));
+		this.buttonPanel.setBackground(Color.BLUE);
 	}
 
 	@Override
@@ -108,5 +151,14 @@ public class Interface extends GamePanel {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	/**
+	 * Inner class that allows for print listening.
+	 */
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			//
+		}
 	}
 }
